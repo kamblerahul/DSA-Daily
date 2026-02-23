7d653ac6784d6b0a0044e132adc9d9c6451972c8
kamblerahul <rahul.kamble1093@gmail.com>
2026-02-23 12:31:20 +0530

    Factorial : Recursive Appraoch

--- src/main/java/com/dsa/mathematics/FactorialNumber.java
+++ src/main/java/com/dsa/mathematics/FactorialNumber.java
@@ -2,20 +2,28 @@ package com.dsa.mathematics;
 
 public class FactorialNumber {
 
-
     public static void main(String[] args) {
         System.out.println(factorialIterative(4));
+        System.out.println(factorialRecursive(5));
     }
 
-    public static int factorialIterative(int inputNumber){
+    //Iterative Appraoch
+    public static int factorialIterative(int inputNumber) {
         int fact = 1;
-            if(inputNumber == 0 || inputNumber == 1){
-                return 1;
-            }
-            for(int i = 2; i<=inputNumber ;i++){
-                fact = fact * i;
-            }
-        return fact;
+        if (inputNumber == 0 || inputNumber == 1) {
+            return 1;
         }
+        for (int i = 2; i <= inputNumber; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    //Recursive Approach
+    public static int factorialRecursive(int inputNumber){
+        if(inputNumber==0)
+            return 1;
+        return inputNumber * factorialRecursive(inputNumber-1);
+    }
 
 }
