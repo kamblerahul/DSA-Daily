b2a38c3a28711b4c6dd78963790189c8556a4cbb
rahulkamble <rahul.kamble1093@gmail.com>
2026-05-05 07:46:33 +0530

    recursion 1 to N

---
+++ src/main/java/com/dsa/Recursion/PrintNTo1.java
@@ -0,0 +1,15 @@
+package com.dsa.Recursion;
+
+public class PrintNTo1 {
+
+    public static void main(String[] args) {
+        printNTo1(5);
+    }
+
+    static void printNTo1(int n){
+        if(n==0)
+            return;
+        System.out.println(n);
+        printNTo1(n-1);
+    }
+}
